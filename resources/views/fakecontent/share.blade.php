8a1745b3891de1494d0ae49b0d516a240e85d9b8
John Nguyen <vnzacky39@gmail.com>
2016-03-09 00:35:39 +0700

    fix fb og URL

--- resources/views/fakecontent/share.blade.php
+++ resources/views/fakecontent/share.blade.php
@@ -9,7 +9,7 @@
     <meta property="og:site_name" content="Video"/>
     <meta property="og:title" content="{{ $content->name }}"/>
     <meta property="og:description" content="{{ $content->description }}"/>
-    <meta property="og:url" content="http://zing.vn"/>
+    <meta property="og:url" content="{{ \URL::full() }}"/>
     <meta property="og:image" content="{{ $content->image }}"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 </head>
